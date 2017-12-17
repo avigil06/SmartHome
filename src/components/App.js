25d3166ca13777f876bfea0ef2e08c379891a601
Andrew <avigil06@gmail.com>
2017-12-16 21:48:45 -0500

    build of auth redirect logic

---
+++ src/components/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import { Switch, Route, Match, Redirect } from 'react-router-dom'
+import { injectGlobal, ThemeProvider } from 'styled-components'
+
+import { HomePage, SamplePage, NotFoundPage } from 'components'
+import { GoogleTagManager } from 'containers'
+
+import { auth, storageKey, isAuthenticated } from '../services/auth'
+
+// https://github.com/diegohaz/arc/wiki/Styling
+import theme from './themes/default'
+
+injectGlobal`
+  body {
+    margin: 0;
+  }
+`
+
+const MatchWhenAuthorized = ({component: Component, ...rest}) => (
+  <Route {...rest} render={renderProps => (
+    isAuthenticated() ? (
+      <Component {...renderProps} />
+    ) : (
+      <Redirect to={ {
+        pathname: '/login',
+        state: {from: renderProps.location}
+      } } />
+    )
+  )}/>
+)
+
+class App extends Component {
+
+  state = {
+    uid: null
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        window.localStorage.setItem(storageKey, user.uid)
+        this.setState({uid: user.uid})
+      } else {
+        window.localStorage.removeItem(storageKey)
+        this.setState({uid: null})
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <GoogleTagManager />
+        <ThemeProvider theme={theme}>
+          <Switch>
+            <Route path="/" component={HomePage} exact />
+            <MatchWhenAuthorized path="/sample-page" component={SamplePage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </ThemeProvider>
+      </div>
+    )
+  }
+
+}
+
+export default App
